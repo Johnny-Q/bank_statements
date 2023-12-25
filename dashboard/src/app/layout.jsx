1c77399711ea63ca4b996bfbca85dfeae64104a0
Johnny-Q <johnnyqu4@gmail.com>
2023-12-25 18:25:24 -0500

    add basic next app

---
+++ dashboard/src/app/layout.jsx
@@ -0,0 +1,13 @@
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
