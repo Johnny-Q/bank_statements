2cbe121f3265957d5fc120d2bff4ceab93d0dc43
Johnny-Q <johnnyqu4@gmail.com>
2023-12-25 21:13:13 -0500

    set up authorization flow for Google Sheets API

--- dashboard/src/app/api/auth/[...nextauth]/route.js
+++ dashboard/src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,36 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 export const handler = NextAuth({
-    // Configure one or more authentication providers
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
+            authorization: {
+                params: {
+                    scope: 'https://www.googleapis.com/auth/spreadsheets email openid',
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         }),
-        // ...add more providers here
-    ],
+    ], callbacks: {
+        async session(params) {
+            console.log("session", params);
+            const {session} = params;
+            return session;
+        },
+        async jwt(params) {
+            console.log("jwt", params);
+            const {token} = params;
+            return token;
+        },
+        async signIn(params) {
+            console.log("signIn", params);
+            const {account} = params
+            //use could reject the option to allow google sheets, app needs to handle that
+            return true;
+        }
+    }
 })
 export { handler as GET, handler as POST }
