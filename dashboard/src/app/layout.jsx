57b9040a5b194126471ff4076f0c5b7340fdbf5b
Johnny-Q <johnnyqu4@gmail.com>
2023-12-31 18:15:24 -0500

    remove session provider from root layout

--- dashboard/src/app/layout.jsx
+++ dashboard/src/app/layout.jsx
@@ -1,17 +1,14 @@
 import Nav from "@/components/Nav"
-import Provider from "@/components/Provider"
 
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
-            <Provider>
-                <body>
-                    <Nav />
-                    <main className="app">
-                        {children}
-                    </main>
-                </body>
-            </Provider>
+            <body>
+                <Nav />
+                <main className="app">
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
