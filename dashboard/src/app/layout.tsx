340456ad4ed43b5bd7b868ceaa71af5260e77835
Johnny-Q <johnnyqu4@gmail.com>
2024-01-01 19:38:55 -0500

    add shadcn

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,13 +1,22 @@
+import "@/styles/globals.css";
 import Nav from "@/components/Nav"
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
-            <body>
-                <Nav />
-                <main className="app">
-                    {children}
-                </main>
+            <body className="flex flex-row h-screen">
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Nav />
+                    <main className="grow flex flex-col justify-center items-center px-6">
+                        {children}
+                    </main>
+                </ThemeProvider>
             </body>
         </html>
     )
