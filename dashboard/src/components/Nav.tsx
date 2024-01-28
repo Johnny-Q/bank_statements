9f9bf881529bcf11b19cb62b9a653ac6646e5873
Johnny-Q <johnnyqu4@gmail.com>
2024-01-28 17:38:39 -0500

    make nav links into buttons

--- dashboard/src/components/Nav.tsx
+++ dashboard/src/components/Nav.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link"
+import { Button } from "./ui/button"
 const Nav = () => {
   return (
     <nav className="grid-col-start-1 col-span-1 h-full flex flex-col">
-      <Link href="/test">
-        Dashboard
-      </Link>
-      <Link href="/add-transaction">
-        Add Transaction
-      </Link>
-      <Link href="/upload-transactions">
-        Upload CSV
-      </Link>
+      <Button variant="ghost" asChild className="items-start justify-start">
+        <Link href="/dashboard">
+          Dashboard
+        </Link>
+      </Button>
+
+      <Button variant="ghost" asChild className="items-start justify-start">
+        <Link href="/add-transaction">
+          Add Transaction
+        </Link>
+      </Button>
+
+      <Button variant="ghost" asChild className="items-start justify-start">
+        <Link href="/upload-transactions">
+          Upload CSV
+        </Link>
+      </Button>
     </nav>
   )
 }
