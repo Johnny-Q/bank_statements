7fbc9924e5e2330c5273c199caa58a67072e7215
Johnny-Q <johnnyqu4@gmail.com>
2024-01-28 14:37:58 -0500

    add logic to handle empty sheet

--- dashboard/src/app/dashboard/page.tsx
+++ dashboard/src/app/dashboard/page.tsx
@@ -1,26 +1,53 @@
 import TransactionsTable from "@/components/TransactionsTable";
 import { Progress } from "@/components/ui/progress";
 import { useSheetsClient } from "@/utils/hooks";
-import CategoryOverviewCard from "./CategoryOverviewCard";
+import { Transaction } from "@/utils/transactionValidator";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+
 
 const DashboardPage = async () => {
     const sheets = await useSheetsClient();
 
     const today = new Date();
-    const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
-    const endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-    const total_spending = await sheets.getTotalSpending({startDate, endDate});
-    const recent_transactions = await sheets.getTransactions({ limit: 5 });
-    const spending_by_category = await sheets.getCategorySpending({startDate, endDate});
+    const start_date = new Date(today.getFullYear(), today.getMonth(), 1);
+    const end_date = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
     const MAX_SPENDINGS = 500;
 
+    //run this first to check if there is any data in the spreadsheet (no data will cause problems for other queries)
+    let recent_transactions: Transaction[] = [];
+    let total_spending = 0;
+    let spending_by_category = {}
+    let error = true;
+    try {
+        recent_transactions = await sheets.getTransactions({ limit: 5 });
+        if (recent_transactions.length > 0) { //these queries only work if the user has already added some data to the spreadsheet
+            spending_by_category = await sheets.getCategorySpending({ startDate: start_date, endDate: end_date });
+            total_spending = 0; Math.abs(await sheets.getTotalSpending({ startDate: start_date, endDate: end_date }));
+        }
+        error = false;
+    } catch (err) {
+        console.error(err);
+
+        //reset variables
+        recent_transactions = [];
+        total_spending = 0;
+        spending_by_category = {}
+    }
+
+
     return (
         <div className="w-full px-5">
+            {error &&
+                (<Alert variant="destructive">
+                    <AlertDescription>
+                        Error loading transactions.
+                    </AlertDescription>
+                </Alert>)
+            }
             <section className="my-5">
-                <h1 className="text-xl">You've Spent</h1>
-                <h1 className="text-6xl">${Math.abs(total_spending).toFixed(2)}</h1>
+                <h1 className="text-xl">This month, you've spent</h1>
+                <h1 className="text-6xl">${total_spending.toFixed(2)}</h1>
                 <div>
                     <Progress value={total_spending / MAX_SPENDINGS * 100} className="h-3 w-full" />
                     <div className="w-full flex flex-row justify-between">
--- dashboard/src/utils/SheetsClient.ts
+++ dashboard/src/utils/SheetsClient.ts
@@ -33,6 +33,58 @@ export default class SheetsClient {
         });
     }
 
+    /* 
+    compares given transactions with the ones already in the sheet
+    */
+    async compareTransactions(transactions: any[]): Promise<any[]> {
+
+        //sort the transactions by date
+        transactions.sort((a: any, b: any) => {
+            if (a["date"] > b["date"]) return -1;
+            if (a["date"] < b["date"]) return 1;
+            return b.amount - a.amount;
+        });
+        const end_date = new Date(transactions[0]["date"]);
+        const start_date = new Date(transactions[transactions.length - 1]["date"]);
+        const sheet_transactions = await this.getTransactions({ start_date, end_date });
+
+        //sort using the same algorithm
+        sheet_transactions.sort((a: any, b: any) => {
+            if (a["date"] > b["date"]) return -1;
+            if (a["date"] < b["date"]) return 1;
+            return b.amount - a.amount;
+        });
+
+        //compare the two
+        let iter_a = 0, iter_b = 0;
+        const new_transactions: any[] = [];
+        while (iter_a < transactions.length && iter_b < sheet_transactions.length) {
+            if (transactions[iter_a]["date"] === sheet_transactions[iter_b]["date"]) {
+                if (Math.abs(transactions[iter_a]["amount"] - sheet_transactions[iter_b]["amount"]) < 0.001) {
+                    iter_a++;
+                    iter_b++;
+                } else if (transactions[iter_a]["amount"] > sheet_transactions[iter_b]["amount"]) {
+                    new_transactions.push(transactions[iter_a]);
+                    iter_a++;
+                } else {
+                    iter_b++;
+                }
+            } else if (transactions[iter_a]["date"] > sheet_transactions[iter_b]["date"]) {
+                new_transactions.push(transactions[iter_a]);
+                iter_a++;
+            } else {
+                iter_b++;
+            }
+        }
+
+        while (iter_a < transactions.length) {
+            new_transactions.push(transactions[iter_a]);
+            iter_a++;
+        }
+
+        return new_transactions;
+    }
+
     async appendTransactions(transactions: any[]): Promise<void> {
         const res = await this.sheets.spreadsheets.values.append({
             spreadsheetId: this.spreadsheet_id,
@@ -51,34 +103,39 @@ export default class SheetsClient {
         })
     }
 
-    async getTransactions(options?: { startDate?: Date, endDate?: Date, limit?: number }) {
-        let query = "select * "
-
-        if (options?.startDate && options?.endDate) {
+    async getTransactions(options?: { start_date?: Date, end_date?: Date, limit?: number }) {
+        let query = "select *"
+        if (options?.start_date && options?.end_date) {
             // Both dates are provided
-            query += ` where date '${options.startDate.toISOString().substring(0, 10)}' <= A and A <= date '${options.endDate.toISOString().substring(0, 10)}'`;
-        } else if (options?.startDate) {
+            query += ` where date '${options.start_date.toISOString().substring(0, 10)}' <= A and A <= date '${options.end_date.toISOString().substring(0, 10)}'`;
+        } else if (options?.start_date) {
             // Only start date is provided
-            query += ` where date '${options.startDate.toISOString().substring(0, 10)}' <= A`;
-        } else if (options?.endDate) {
+            query += ` where date '${options.start_date.toISOString().substring(0, 10)}' <= A`;
+        } else if (options?.end_date) {
             // Only end date is provided
-            query += ` where A <= date '${options.endDate.toISOString().substring(0, 10)}'`;
+            query += ` where A <= date '${options.end_date.toISOString().substring(0, 10)}'`;
         }
 
-        query += "order by A desc";
+        query += " order by A desc";
 
         if (options?.limit) {
             query += ` limit ${options.limit}`;
         }
+        const res = await this.query(query);
 
         //convert dates to date objects
-
-        const res = await this.query(query);
-        // console.log(res);
         const transactions = res.map((entry: any) => {
+            //parse the date
+            //Date usually has the form "Date(year,month,date)" (if the user didn't change anything)
+            let [year, month, day] = entry["DATE"].substring(5, entry["DATE"].length - 1).split(",");
+            month = parseInt(month);
+            month = month + 1;
+
+
             return {
-                "DESCRIPTION": entry["DESCRIPTION"] || entry["BANK DESCRIPTION"],
-                "AMOUNT": entry["AMOUNT"]
+                "date": `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`,
+                "description": entry["DESCRIPTION"] || entry["BANK DESCRIPTION"],
+                "amount": entry["AMOUNT"]
             }
         })
         return transactions;
@@ -99,8 +156,11 @@ export default class SheetsClient {
         }
 
         const res = await this.query(query);
-        // console.log(res);
-        return res[0]["sum AMOUNT"];
+        if (res.length > 0) {
+            return res[0]["sum AMOUNT"];
+        } else {
+            return 0;
+        }
     }
 
     async getCategorySpending(options?: { startDate?: Date, endDate?: Date }): Promise<{ [category: string]: number }> {
@@ -118,7 +178,6 @@ export default class SheetsClient {
         query += "group by E";
 
         const res = await this.query(query);
-        // console.log(res);
         const spendingByCategory: { [category: string]: number } = {};
         res.forEach((entry: any) => {
             const category = entry["CATEGORY"] || "Uncategorized";
@@ -138,19 +197,27 @@ export default class SheetsClient {
             `&access_token=${encodeURIComponent((await this.auth.getAccessToken()).token as string)}`;
 
         const resp = await fetch(url);
-        if (resp.status != 200) {
-            console.log(resp.status);
-            return []
+
+        if (!resp.ok) {
+            console.error("Query fetch not OK");
+            throw "Query fetch not OK";
         }
-        const jsonData = extractDataFromQueryResponse(await resp.text());
-        // return jsonData.table;
 
-        // Extract the table data
+        let jsonData = extractDataFromQueryResponse(await resp.text()); //could throw an error
+
+        if (jsonData.status === "error") {
+            console.log(`Query received error response: ${JSON.stringify(jsonData.errors)}`);
+            throw `Query received error response`;
+        }
+        
         const tableData = jsonData.table;
-
         const columns = tableData.cols.map((col: any) => col.label);
 
-        // Extract rows
+        //if the column labels are empty, then there is only a single row of column titles in the sheet
+        if (columns.some((label: string) => label === "")) {
+            return []; //sheets had no transactions
+        }
+
         const rows = tableData.rows.map((row: any) => {
             const ret: any = {};
             row.c.forEach((cell: any, i: number) => {
